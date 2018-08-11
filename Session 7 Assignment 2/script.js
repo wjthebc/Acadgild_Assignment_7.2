0ee0e7ac753b51ea822c1d2653176d1849839d50
wjthebc <40471359+wjthebc@users.noreply.github.com>
2018-08-11 13:57:49 -0400

    Add files via upload

---
+++ Session 7 Assignment 2/script.js
@@ -0,0 +1,15 @@
+//logs multples of 5 through 50 using for loop
+console.log('Using for loop')
+for(var i=5; i < 51; i+=5)
+  {
+    console.log(i)
+  }
+
+//logs multuples of 5 through 50 using while loop
+console.log('Using while loop')
+var x = 5;
+while(x < 51)
+  {
+    console.log(x);
+    x+=5;
+  }
